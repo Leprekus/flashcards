961c682a5639c0cdbf1860e04c5658b0e0666d5c
leprekus <leprekus@yahoo.com>
2022-10-18 18:47:33 -0700

    initial commit

---
+++ flashcards-starter/src/features/cards/cardSlice.js
@@ -0,0 +1,19 @@
+//   cards: {
+//     cards: {
+//       '789': {
+//         id: '789',
+//         front: 'front text',
+//         back: 'back text'
+//       },
+//       '101': {
+//         id: '101',
+//         front: 'front text',
+//         back: 'back text'
+//       },
+//       '102': {
+//         id: '102',
+//         front: 'front text',
+//         back: 'back text'
+//       },
+//     }
+//   }
---
+++ flashcards-starter/src/features/topics/topicsSlice.js
@@ -0,0 +1,10 @@
+//   topics: {
+//     topics: {
+//       '123': {
+//         id: '123',
+//         name: 'example topic',
+//         icon: 'icon url',
+//         quizIds: ['456']
+//       }
+//     }
+//   },
